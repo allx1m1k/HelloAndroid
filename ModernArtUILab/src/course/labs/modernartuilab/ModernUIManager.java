7a658f967a334a4d45664ebc4f32e42d0585fd36
allx1m1k <dima@ua2012.org.ua>
2014-10-27 01:31:31 +0200

    AskDialogFragment created and forced to invoke when user clicks on Option menu

--- ModernArtUILab/src/course/labs/modernartuilab/ModernUIManager.java
+++ ModernArtUILab/src/course/labs/modernartuilab/ModernUIManager.java
@@ -1,6 +1,10 @@
 package course.labs.modernartuilab;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +16,8 @@ import android.widget.Toast;
 public class ModernUIManager extends Activity {
 
     static private final String TAG = "ModernUIManager";
+    private static final int ALERTTAG = 0;
+    private DialogFragment mDialog;
     /**
      * Called when the activity is first created.
      */
@@ -36,17 +42,90 @@ public class ModernUIManager extends Activity {
     // Process clicks on Options Menu items
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "Entered onOptionsItemSelected");
         switch (item.getItemId()) {
             case R.id.moreInfo:
                 Toast.makeText(getApplicationContext(), "User have asked for more info. Dialog have to appear!",
                         Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.userDialog:
-                Toast.makeText(getApplicationContext(), "Dialog starts!",
-                        Toast.LENGTH_SHORT).show();
+                showDialogFragment(ALERTTAG);
                 return true;
             default:
                 return false;
         }
     }
+
+    // Show desired Dialog
+    void showDialogFragment(int dialogID) {
+        Log.i(TAG, "Entered showDialogFragment");
+        switch (dialogID) {
+            // Show AlertDialog
+            case ALERTTAG:
+                // Create a new AlertDialogFragment
+                mDialog = AskDialogFragment.newInstance();
+                // Show AlertDialogFragment
+                mDialog.show(getFragmentManager(), "Alert");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Abort or complete ShutDown based on value of shouldContinue
+    private void continueWork(boolean shouldContinue) {
+        Log.i(TAG, "Entered continueWork");
+/*        if (shouldContinue) {
+
+            // Prevent further interaction with the ShutDown Butotn
+            mShutdownButton.setEnabled(false);
+
+            // Show ProgressDialog as shutdown process begins
+            showDialogFragment(PROGRESSTAG);
+
+            // Finish the ShutDown process
+            finishShutdown();
+
+        } else { */
+
+            // Abort ShutDown and dismiss dialog
+            mDialog.dismiss();
+/*        } */
+    }
+
+    // Class that creates the AskDialog
+    public static class AskDialogFragment extends DialogFragment {
+
+        public static AskDialogFragment newInstance() {
+            Log.i(TAG, "New AskDialogFragment created");
+            return new AskDialogFragment();
+        }
+        // Build AlertDialog using AlertDialog.Builder
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Log.i(TAG, "Entered onCreateDialog");
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage("Do you really want to exit?")
+
+                            // User cannot dismiss dialog by hitting back button
+                    .setCancelable(false)
+
+                            // Set up No Button
+                    .setNegativeButton("No",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    ((ModernUIManager) getActivity()).continueWork(false);
+                                }
+                            })
+
+                            // Set up Yes Button
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        final DialogInterface dialog, int id) {
+                                    ((ModernUIManager) getActivity()).continueWork(true);
+                                }
+                            }).create();
+        }
+    }
+
 }

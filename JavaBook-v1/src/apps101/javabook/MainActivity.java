dae06af83a02e30725f7e9807a329b3bb38c06ad
allx1m1k <dima@ua2012.org.ua>
2014-11-22 01:11:58 +0200

    JavaBook java script enabled (see video and lesson Week5)

--- JavaBook-v1/src/apps101/javabook/MainActivity.java
+++ JavaBook-v1/src/apps101/javabook/MainActivity.java
@@ -13,8 +13,16 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		// Find that webView1
 		WebView pointer = (WebView) findViewById(R.id.webView1);
+        //enable zoom controls and JavaScript
+        pointer.getSettings().setBuiltInZoomControls(true);
+        pointer.getSettings().setJavaScriptEnabled(true);
+
 		// Open asset/index.html
 		pointer.loadUrl("file:///android_asset/index.html");
+
+        //my simple html page
+        //pointer.loadUrl("file:///android_asset/myIndex.html");
+
 	}
 
 	@Override

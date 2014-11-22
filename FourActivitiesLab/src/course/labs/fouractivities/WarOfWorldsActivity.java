a2ad55de6658dca1fe02004e780a8ec953005f82
allx1m1k <dima@ua2012.org.ua>
2014-11-22 22:39:33 +0200

    FourActivitiesLab implemented:  - An extract of War of the Worlds by H.G. Wells

--- FourActivitiesLab/src/course/labs/fouractivities/WarOfWorldsActivity.java
+++ FourActivitiesLab/src/course/labs/fouractivities/WarOfWorldsActivity.java
@@ -2,10 +2,10 @@ package course.labs.fouractivities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 
-public class JabberwockyActivity extends Activity {
+
+public class WarOfWorldsActivity extends Activity {
     WebView mWebView;
 
     /**
@@ -14,12 +14,12 @@ public class JabberwockyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_warofworlds);
         //initialize controls
         mWebView = (WebView) findViewById(R.id.webView);
 
         //load web content
-        mWebView.loadUrl("file:///android_asset/jabberwocky.html");
+        mWebView.loadUrl("file:///android_asset/waroftheworlds.html");
 
     }
 }

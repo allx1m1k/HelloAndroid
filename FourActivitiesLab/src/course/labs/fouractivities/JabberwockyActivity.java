bf2d82c246fc7fb556b8074d7ff78ea548142234
allx1m1k <dima@ua2012.org.ua>
2014-11-23 02:50:14 +0200

    Implemented: - mp3 playing only with JabberwockyActivity

--- FourActivitiesLab/src/course/labs/fouractivities/JabberwockyActivity.java
+++ FourActivitiesLab/src/course/labs/fouractivities/JabberwockyActivity.java
@@ -1,12 +1,41 @@
 package course.labs.fouractivities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+
+import java.io.BufferedReader;
 
 public class JabberwockyActivity extends Activity {
-    WebView mWebView;
+    private final static String TAG = "FourActivitiesLab";
+    private MediaPlayer mMediaPlayer;
+    /**
+     * Called when activity is visible
+     */
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "Entered onResume");
+        mMediaPlayer = MediaPlayer.create(this, R.raw.weareone);
+        mMediaPlayer.start();
+        super.onResume();
+    }
+
+    /**
+     * Called when activity become invisible
+     */
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Entered onPause");
+        mMediaPlayer.stop();
+        mMediaPlayer.release();
+        super.onPause();
+    }
 
     /**
      * Called when the activity is first created.
@@ -14,12 +43,38 @@ public class JabberwockyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       Log.i(TAG, "Entered onCreate");
         setContentView(R.layout.main);
         //initialize controls
-        mWebView = (WebView) findViewById(R.id.webView);
+        final WebView mWebView = (WebView) findViewById(R.id.webView);
+        final Button mLeftButton = (Button) findViewById(R.id.buttonLeft);
+        final Button mRightButton = (Button) findViewById(R.id.buttonRight);
 
         //load web content
         mWebView.loadUrl("file:///android_asset/jabberwocky.html");
 
+        //listener for left button - will open the Wikipedia
+        mLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "entered mLeftButton onClick");
+                //open Wikipedia in external browser http://stackoverflow.com/questions/3004515/android-sending-an-intent-to-browser-to-open-specific-url
+                String url = "http://en.wikipedia.org/wiki/Jabberwocky";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            }
+        });
+
+        //listener for right button - will open the
+        mRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open picture with beast
+                Log.i(TAG, "entered mRightButton onClick");
+                mWebView.loadUrl("file:///android_asset/angraves-highscore6x6.png");
+            }
+        });
     }
 }

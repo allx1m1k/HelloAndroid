5eb0c65408c81d0dd01e8a3361f679af69915771
allx1m1k <dima@ua2012.org.ua>
2014-11-24 01:39:39 +0200

    MyMapsDemo

--- MyMapsDemo/src/com/example/mapdemo/MyMapsActivity.java
+++ MyMapsDemo/src/com/example/mapdemo/MyMapsActivity.java
@@ -2,8 +2,11 @@ package com.example.mapdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MyMapsActivity extends Activity {
+public class MyMapsActivity extends FragmentActivity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +14,7 @@ public class MyMapsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        GoogleMap map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView)).getMap();
     }
 }

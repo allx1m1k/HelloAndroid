e93c1daa66edf89f1605b0005ce026024f128697
allx1m1k <dima@ua2012.org.ua>
2014-11-25 23:39:44 +0200

    implemented Google Maps but can not start

--- MyMapsDemo/src/com/example/mapdemo/MyMapsActivity.java
+++ MyMapsDemo/src/com/example/mapdemo/MyMapsActivity.java
@@ -1,20 +1,44 @@
 package com.example.mapdemo;
+import com.google.android.gms.maps.*;
+        import com.google.android.gms.maps.model.*;
+        import android.app.Activity;
+        import android.os.Bundle;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+public class MyMapsActivity extends Activity {
 
-public class MyMapsActivity extends FragmentActivity {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        GoogleMap map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView)).getMap();
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapView)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
     }
+
+    /** https://developers.google.com/maps/documentation/android/start
+     * package com.example.mapdemo;
+
+     import android.app.Activity;
+     import android.os.Bundle;
+
+     public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    }
+    }
+     */
 }

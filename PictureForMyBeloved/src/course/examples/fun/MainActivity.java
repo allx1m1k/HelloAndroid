18049c206b946bce30643caa556098eec41b6b4e
allx1m1k <dima@ua2012.org.ua>
2014-12-07 16:17:02 +0200

    PictureForMyBeloved implemented  - Runnable  - Activity has to be finished after 3 sec.

--- PictureForMyBeloved/src/course/examples/fun/MainActivity.java
+++ PictureForMyBeloved/src/course/examples/fun/MainActivity.java
@@ -1,44 +1,36 @@
 package course.examples.fun;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
-import course.examples.fun.R;
+
 
 public class MainActivity extends Activity {
     private final static String TAG = "PictureForMyBeloved";
     private ImageButton mImageButton;
-    private Intent intent;
-    private Intent emailIntent;
-    //final String mFB = "https://en.wikipedia.org/wiki/Facebook";
-    //final String mGgl = "https://en.wikipedia.org/wiki/Google";
     //initial selector value is relevant to R.drawable.flower320x480
     private int selector = 0;
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate " + selector);
+        Log.i(TAG, "Entered onCreate " + selector);
         setContentView(R.layout.main);
         mImageButton = (ImageButton) findViewById(R.id.imageButton);
         //setting the first image and url - is facebook
         mImageButton.setImageResource(R.drawable.flower320x480);
-        intent = new Intent(Intent.ACTION_VIEW);
-
 
         //setOnClickListener in order to display desirable image
         mImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, "onClick " + selector);
+                Log.i(TAG, "Entered onClick " + selector);
                 //the logic here is to evaluate the flag selector
                 //and based on evaluated value we have to display the image
                 // selector = 0 -> R.drawable.flower320x480
@@ -60,6 +52,7 @@ public class MainActivity extends Activity {
             @Override
             public boolean onLongClick(View v) {
                 Log.i(TAG, "onLongClick " + selector);
+                //we call Email client
                 sendEmail();
                 return true;
             }
@@ -68,9 +61,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Called when Activity recieved onPause callback
+     */
     @Override
     protected void onPause() {
-        Log.i(TAG, "onPause " + selector);
+        Log.i(TAG, "Entered onPause " + selector);
         // evaluate the selector and change its value
         switch (selector) {
             case 0:
@@ -79,40 +75,50 @@ public class MainActivity extends Activity {
             case 1:
                 selector = 0;
         }
-        mImageButton.postDelayed(r, 3000);
-        SystemClock.sleep(3000);
         super.onPause();
-
     }
 
-    Runnable r = new Runnable() {
-        @Override
-        public void run() {
-            Log.i(TAG, "Entered run");
-        }
-    };
+    /**
+     * Called when Activity recieved onResume callback
+     */
     @Override
     protected void onResume() {
-        Log.i(TAG, "onResume " + selector);
+        Log.i(TAG, "Entered onResume " + selector);
         // change bitmap for button based on selector value
         if (selector == 0) {
-            //intent.setData(Uri.parse(mFB));
             mImageButton.setImageResource(R.drawable.flower320x480);
-            //startActivity(intent);
         } else {
-            //intent.setData(Uri.parse(mGgl));
             mImageButton.setImageResource(R.drawable.flower480x800);
-            //startActivity(intent);
         }
         super.onResume();
     }
 
+    /**
+     * Called when Activity recieved onBackPressed callback
+     */
+    @Override
+    public void onBackPressed() {
+        Log.i(TAG, "Entered onBackPressed");
+        //the logic here is to finish Activity the 3 after sec when user pressed Back button
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "Entered run");
+                finish();
+            }
+        };
+        mImageButton.postDelayed(r, 3000);
+    }
+
+    /**
+     * Called when Email has to be sent
+     */
     protected void sendEmail() {
         Log.i(TAG, "Send email");
-        String[] TO = {"dima@gmail.com"};
+        String[] TO = {"dima@ua2012.org.ua"};
         String subject = "To my Beloved";
         String body = "I love you!";
-
+        //Create an Intent to
         final Intent result = new Intent(android.content.Intent.ACTION_SEND);
         result.setType("plain/text");
         result.putExtra(android.content.Intent.EXTRA_EMAIL, TO);
@@ -121,49 +127,10 @@ public class MainActivity extends Activity {
         try {
             Intent mailer = Intent.createChooser(result, null);
             startActivity(mailer);
-            //startActivity(Intent.createChooser(result, "Send mail..."));
-            //finish();
             Log.i("Finished sending email...", "");
         } catch (android.content.ActivityNotFoundException ex) {
             Toast.makeText(MainActivity.this,
                     "There is no email client installed.", Toast.LENGTH_SHORT).show();
         }
-/*
-        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                "mailto","abc@gmail.com", null));
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "EXTRA_SUBJECT");
-        emailIntent.setType("message/rfc822");
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send email..."));
-            finish();
-            Log.i(TAG, "Finished sending email...");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this,
-                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
-        }
-*/
-/*
-        String[] TO = {"dima@gmail.com"};
-        //String[] CC = {"mcmohd@gmail.com"};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-
-
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
-
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            finish();
-            Log.i("Finished sending email...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this,
-                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
-        }
-*/
     }
-
 }

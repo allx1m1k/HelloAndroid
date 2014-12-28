945699cee2681f538da03b3d09b0a165b18960f9
allx1m1k <dima@ua2012.org.ua>
2014-12-28 03:32:19 +0200

    BukovelSnowIt src

---
+++ BukovelSnowIt/src/ua/org/ua2012/BukovelSnowItActivity.java
@@ -0,0 +1,15 @@
+package ua.org.ua2012;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BukovelSnowItActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

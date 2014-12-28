5049f45439dfa2c4edd04ee3c4632a78ac652a81
allx1m1k <dima@ua2012.org.ua>
2014-12-28 03:31:41 +0200

    BukovelSnowIt src

---
+++ BukovelSnowIt/src/ua/org/ua2012/SnowReportRec.java
@@ -0,0 +1,9 @@
+package ua.org.ua2012;
+
+/**
+ * Created by dima on 12/28/2014.
+ */
+public class SnowReportRec {
+    int mountainSnowLevel; //<schneehoehe_berg>
+    int valleySnowLevel; //<schneehoehe_tal>
+}

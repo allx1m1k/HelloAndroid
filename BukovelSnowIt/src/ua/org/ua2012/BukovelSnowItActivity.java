8b5d12eac6957ad9fa0bc66a0957626c14251f9c
allx1m1k <dima@ua2012.org.ua>
2015-01-02 17:10:58 +0200

    Taken some code from NetworkingAndroidHttpClientXML

--- BukovelSnowIt/src/ua/org/ua2012/BukovelSnowItActivity.java
+++ BukovelSnowIt/src/ua/org/ua2012/BukovelSnowItActivity.java
@@ -1,15 +1,27 @@
 package ua.org.ua2012;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BukovelSnowItActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+
+        final Button loadButton = (Button) findViewById(R.id.button1);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BukovelSnowItActivity.this, NetworkingAndroidHttpClientXMLActivity.class));
+            }
+        });
     }
 }
